2f2077aac2a03fb87f5d243cea4cb2da949154e8
descholar <emmanuellamugi@gmail.com>
2020-05-26 19:30:26 +0200

    feat: Validation
    -Add custom error messages
    
    [Finishes#]

--- src/helpers/customMessages.js
+++ src/helpers/customMessages.js
@@ -7,4 +7,14 @@ export default {
     3. Secure your digital property 
     `,
   },
+  errorMessages: {
+    firstnameErr: 'First name should be text, without spaces, no special characters, and no numbers and should not be empty',
+    lastnameErr: 'First name should be text, without spaces, no special characters, and no numbers and should not be empty',
+    usernameErr: 'Username should be text, without spaces, no special characters, and no numbers, and no more than 50 characters and should not be empty',
+    passwordErr: 'Password should not be empty, made of letters, atleast on capital letter, atleast on number, and atleast on special character',
+    ageErr: 'Age should be a number specifying your year of birth eg:1999',
+    emailErr: 'Email should not be empty, and valid email address',
+    sexErr: 'Sex should be male or female',
+    addressErr: 'Address should not be empty, in form of (country, city) eg: (USA, New York)',
+  },
 };
