cc1c26b5bc739a00dc88f0faa75cabf509d73791
descholar <emmanuellamugi@gmail.com>
2020-06-01 19:05:53 +0200

    feat: user logout
    -Add a controller to logout
    
    [Finishes#173007364]

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import morgan from 'morgan';
 import cookieSession from 'cookie-session';
 import passport from './passport/social.passport.config';
 import allRoutes from './routes/index';
+import nezamediaScheduler from './database/redis/taskerScheduler';
 
 dotenv.config();
 
@@ -11,7 +12,7 @@ const { PORT } = process.env;
 const server = express();
 
 server.use(express.json());
-// server.use(morgan('combined'));
+server.use(morgan('combined'));
 server.use(cookieSession({
   name: 'nezamediaSession',
   keys: ['key1'],
@@ -23,6 +24,7 @@ server.use(allRoutes);
 server.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`NezaMedia is running on port ${PORT}`);
+  nezamediaScheduler.start();
 });
 
 export default server;
